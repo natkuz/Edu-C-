21f9e96d2529053e5981f41a10952f381b0749ac
NataKuz <nv8282@mail.ru>
2022-10-16 10:48:40 +0800

    Added Hello_favirite progect whith if-else

---
+++ Hello_favorite/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")// ToLower - Переводит все символы в нижний регистр
+{
+    Console.WriteLine("Ура, к нам Маша пришла!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
