4757a96cf016683a1084c90b4d2c45caf7f0fe5a
NataKuz <nv8282@mail.ru>
2022-10-16 10:18:57 +0800

    Changed Sum project with random numbers

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;//переменная numberA имеет тип целого числа integer и принимает значение 3
-int numberB = 25;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
