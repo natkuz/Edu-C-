c42f4d3820187df9e7d5cdee18266426eb290e27
NataKuz <nv8282@mail.ru>
2022-10-16 09:47:26 +0800

    added Sum project

--- Hello, user!/Program.cs
+++ Hello, user!/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Введите Ваше имя: ");
-string username = Console.ReadLine();  //в переменную username типа строка записываем данные, которые будут считаны с введенных в терминале данных
+string username = Console.ReadLine();//в переменную username типа строка записываем данные, которые будут считаны с введенных в терминале данных
 Console.Write("Привет, ");
 Console.Write(username);
---
+++ Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 3;//переменная numberA имеет тип целого числа integer и принимает значение 3
+int numberB = 25;
+int result = numberA + numberB;
+Console.WriteLine(result);
